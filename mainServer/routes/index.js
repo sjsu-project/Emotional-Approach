bb514e70b281ae705b227945fa293057fd6f9d65
Hwan-seok <tttkhs@khu.ac.kr>
2019-07-22 17:11:46 +0900

    스토리지 추가 기능

--- mainServer/routes/index.js
+++ mainServer/routes/index.js
@@ -24,6 +24,21 @@ router
                 res.status(400).json({ err });
             });
     })
+    .post(async (req, res) => {
+        const userId = req.params.userId;
+        const now = date.format(new Date(), "YYYY/MM/DD");
+        const expired = date.format(date.addYears(new Date(), 1), "YYYY/MM/DD");
+
+        const maxKey = await fileRepository.selectMaxKey(userId);
+
+        fileRepository
+            .insertStorage(userId, req.body, now, expired, maxKey + 1)
+            .then(() => res.status(200).json({ result: "success" }))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json({ result: "failed" });
+            });
+    })
 });
 
 module.exports = router;
