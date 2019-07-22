fd932c9962394a48fb80d54f29262e62844db944
Hwan-seok <tttkhs@khu.ac.kr>
2019-07-22 17:11:30 +0900

    aws 버킷 연결
    - config.json 파일과 mysql.js 파일은 제외시킴

---
+++ mainServer/lib/storageS3.js
@@ -0,0 +1,19 @@
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const path = require("path");
+var AWS = require("aws-sdk");
+AWS.config.loadFromPath("keys/config.json");
+const s3 = new AWS.S3();
+
+module.exports = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: "traveloguesjsu", //버켓 이름 넣어줌
+        key: function(req, file, cb) {
+            // 버켓에 담을 이름 설정
+            const filename = Date.now().toString() + "_" + file.originalname;
+            cb(null, filename);
+        },
+        acl: "public-read-write" //권한
+    })
+});
