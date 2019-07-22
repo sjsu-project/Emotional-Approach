a8916cb4aa031d0fa03287c698cc443d3ef02166
Hwan-seok <tttkhs@khu.ac.kr>
2019-07-22 17:21:37 +0900

    디펜던시 잘못한부분 수정

--- mainServer/routes/users.js
+++ mainServer/routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+    const result = "";
+    res.status(200).json({
+        result
+    });
 });
 
 module.exports = router;
