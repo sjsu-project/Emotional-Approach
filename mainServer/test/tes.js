90dac0ce2dca821488d56b5203a5ebe44d5d74b6
Hwan-seok <tttkhs@khu.ac.kr>
2019-08-01 18:20:14 +0900

    lock remove

---
+++ mainServer/test/tes.js
@@ -0,0 +1,4 @@
+const str =
+    "{asfd: ['first', 'second', 'third'], 'https://traveloguesjsu.s3.us-west-1.amazonaws.com/first/1564644215762_tttkhs_%EA%B0%95%ED%99%98%EC%84%9D.jpg': ['first', 'second', 'third']}";
+const pa = JSON.parse(str);
+console.log(pa);
